a3983d8d4aa7406ca4431b5931b5b2a34ee0dd66
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2026-01-25 12:01:02 -0500

    Added Stat

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import TeamBattles from "./Components/TeamBattles";
 import Solo from "./Components/Solo";
 import Background from "./Components/Background";
+import { Link } from "react-router-dom";
 
 // &sp=CAI%253D
 
@@ -9,6 +10,14 @@ export default function App() {
     <main className="font-[Ubuntu] text-[12px] md:text-[13px] text-gray-300">
       <Background />
       <img src="gunrox_icon.png" alt="GUNROX ICON" className="mx-auto w-25" draggable={false} />
+      <div className="text-center flex justify-center gap-1">
+        <Link to={`/`}>
+          <div className="p-2 border border-white/20 rounded text-white bg-black px-4">Homepage</div>
+        </Link>
+        <Link to={`/Stats`}>
+          <div className="p-2 border border-white/20 rounded text-white bg-black px-4">Stats</div>
+        </Link>
+      </div>
       {/* Solo */}
       <div className="divider divider-neutral font-[Jersey] text-[24px] text-white my-5">1v1 Battles</div>
       <Solo />
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import App from "./App.jsx";
+import Stats from "./Page/Stats.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/Stats" element={<Stats />} />
       </Routes>
     </Router>
   </StrictMode>,
